b75a00f54f773cc172569fc6426460a17b107a64
DamaiDarma <111102850+DamaiDarma@users.noreply.github.com>
2022-09-08 21:12:34 +0800

    Delete crud directory

--- crud/src/main/java/repository/gururepository.java
+++
@@ -1,8 +0,0 @@
-package repository;
-
-import org.springframework.data.repository.CrudRepository;
-import model.Guru;
-
-public interface gururepository extends CrudRepository<Guru, Integer> {
-    
-}
