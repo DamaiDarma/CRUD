89d3989a7d00a76d8abd74c7d8150bec269abc5f
DamaiDarma <111102850+DamaiDarma@users.noreply.github.com>
2022-09-10 13:07:12 +0800

    Delete crud_project directory

--- crud_project/src/main/java/com/project_crud/crud_project/Repository/JadwalPelajaranRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project_crud.crud_project.Repository;
-import org.springframework.data.repository.CrudRepository;
-import com.project_crud.crud_project.Model.JadwalPelajaran;
-
-public interface JadwalPelajaranRepository extends CrudRepository<JadwalPelajaran, Integer> {
-    
-}
--- crud_project/src/main/java/com/project_crud/crud_project/Repository/VerifikasiAbsenRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project_crud.crud_project.Repository;
-
-import org.springframework.data.repository.CrudRepository;
-import com.project_crud.crud_project.Model.VerifikasiAbsen;
-
-public interface VerifikasiAbsenRepository extends CrudRepository<VerifikasiAbsen, Integer> {
-    
-}
