31d51eb95f281a2d52df4d91b08a65f4ecdb55cd
DamaiDarma <111102850+DamaiDarma@users.noreply.github.com>
2022-09-14 11:25:30 +0800

    Delete crud_project directory

--- crud_project/src/main/java/com/project_crud/crud_project/Repository/AbsenSiswaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project_crud.crud_project.Repository;
-
-import org.springframework.data.repository.CrudRepository;
-import com.project_crud.crud_project.Model.AbsenSiswa;
-
-public interface AbsenSiswaRepository extends CrudRepository<AbsenSiswa, Integer> {
-    
-}
--- crud_project/src/main/java/com/project_crud/crud_project/Repository/PiketRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project_crud.crud_project.Repository;
-
-import org.springframework.data.repository.CrudRepository;
-import com.project_crud.crud_project.Model.Piket;
-
-public interface PiketRepository extends CrudRepository<Piket, Integer> {
-    
-}
