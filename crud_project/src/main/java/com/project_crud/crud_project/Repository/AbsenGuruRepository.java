8f09c66de40530977ace40d7a1ba88e23f1acde7
s-wira <55016501+s-wira@users.noreply.github.com>
2022-09-14 13:35:51 +0800

    Revert "Add files via upload"
    This reverts commit eee165479b23114950fd54ad69992529d20568f6.

--- crud_project/src/main/java/com/project_crud/crud_project/Repository/AbsenGuruRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project_crud.crud_project.Repository;
-
-import org.springframework.data.repository.CrudRepository;
-import com.project_crud.crud_project.Model.AbsenGuru;
-
-public interface AbsenGuruRepository extends CrudRepository<AbsenGuru, Integer> {
-    
-}
--- crud_project/src/main/java/com/project_crud/crud_project/Repository/GuruRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project_crud.crud_project.Repository;
-
-import org.springframework.data.repository.CrudRepository;
-import com.project_crud.crud_project.Model.Guru;
-
-public interface GuruRepository extends CrudRepository<Guru, Integer> {
-    
-}
