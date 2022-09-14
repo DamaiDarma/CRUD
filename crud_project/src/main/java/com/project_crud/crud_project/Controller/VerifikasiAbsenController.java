8e4f0ea7d9ab80f6e81dab44803ce17dee871928
s-wira <55016501+s-wira@users.noreply.github.com>
2022-09-14 13:37:51 +0800

    Revert "Delete crud_project directory"
    This reverts commit 89d3989a7d00a76d8abd74c7d8150bec269abc5f.

---
+++ crud_project/src/main/java/com/project_crud/crud_project/Controller/VerifikasiAbsenController.java
@@ -0,0 +1,75 @@
+package com.project_crud.crud_project.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.project_crud.crud_project.Model.VerifikasiAbsen;
+import com.project_crud.crud_project.Services.VerifikasiAbsenService;
+
+public class VerifikasiAbsenController {
+
+    @Autowired
+	VerifikasiAbsenService VerifikasiAbsenService;
+	
+	
+	 @RequestMapping(value="/list", method=RequestMethod.GET)
+	 public ModelAndView list() {
+		 
+	  ModelAndView model = new ModelAndView("VerifikasiAbsen_list");
+	  List<VerifikasiAbsen> verifikasiabsenList = VerifikasiAbsenService.getAllVerifikasiAbsens();
+	  model.addObject("VerifikasiAbsenList", verifikasiabsenList);
+	
+	  return model;
+	 }
+	 
+	 
+	 
+	 @RequestMapping(value="/addVerifikasiAbsen/", method=RequestMethod.GET)
+	 public ModelAndView addVerifikasiAbsen() {
+		 
+	  ModelAndView model = new ModelAndView();
+	  VerifikasiAbsen VerifikasiAbsen = new VerifikasiAbsen();
+	  model.addObject("VerifikasiAbsenForm", VerifikasiAbsen);
+	  model.setViewName("VerifikasiAbsen_form");
+	  
+	  return model;
+	 }
+	 
+	 
+	 
+	 @RequestMapping(value="/editVerifikasiAbsen/{id}", method=RequestMethod.GET)
+	 public ModelAndView editVerifikasiAbsen(@PathVariable int id) {
+	  ModelAndView model = new ModelAndView();
+	  
+	  VerifikasiAbsen VerifikasiAbsen = VerifikasiAbsenService.getVerifikasiAbsenById(id);
+	  model.addObject("VerifikasiAbsenForm", VerifikasiAbsen);
+	  model.setViewName("VerifikasiAbsen_form");
+	  
+	  return model;
+	 }
+	 
+	 
+	 @RequestMapping(value="/addVerifikasiAbsen", method=RequestMethod.POST)
+	 public ModelAndView add(@ModelAttribute("VerifikasiAbsenForm") VerifikasiAbsen VerifikasiAbsen) {
+		 
+	  VerifikasiAbsenService.addVerifikasiAbsen(VerifikasiAbsen);
+	  return new ModelAndView("redirect:/VerifikasiAbsen/list");
+	  
+	 }
+	 
+	 @RequestMapping(value="/deleteVerifikasiAbsen/{id}", method=RequestMethod.GET)
+	 public ModelAndView delete(@PathVariable("id") int id) {
+		 
+	  VerifikasiAbsenService.deleteVerifikasiAbsen(id);
+	  return new ModelAndView("redirect:/VerifikasiAbsen/list");
+	  
+	 }
+
+
+}
---
+++ crud_project/src/main/java/com/project_crud/crud_project/Model/AbsenGuru.java
@@ -0,0 +1,94 @@
+package com.project_crud.crud_project.Model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "AbsenGuru")
+public class AbsenGuru {
+    
+    @Id
+	 @GeneratedValue(strategy = GenerationType.IDENTITY)
+	 @Column(name ="nik")
+	 private int id;
+	 
+	 @Column(name="id_absen")
+	 private String id_absen;
+	 
+	 @Column(name="jam_datang")
+	 private String jam_datang;
+	 
+     @Column(name="jam_pulang")
+	 private String jam_pulang;
+
+     @Column(name="hari")
+	 private String hari;
+
+     @Column(name="tanggal")
+	 private String tanggal;
+
+     @Column(name="keterangan")
+	 private String keterangan;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getId_absen() {
+        return id_absen;
+    }
+
+    public void setId_absen(String id_absen) {
+        this.id_absen = id_absen;
+    }
+
+    public String getJam_datang() {
+        return jam_datang;
+    }
+
+    public void setJam_datang(String jam_datang) {
+        this.jam_datang = jam_datang;
+    }
+
+    public String getJam_pulang() {
+        return jam_pulang;
+    }
+
+    public void setJam_pulang(String jam_pulang) {
+        this.jam_pulang = jam_pulang;
+    }
+
+    public String getHari() {
+        return hari;
+    }
+
+    public void setHari(String hari) {
+        this.hari = hari;
+    }
+
+    public String getTanggal() {
+        return tanggal;
+    }
+
+    public void setTanggal(String tanggal) {
+        this.tanggal = tanggal;
+    }
+
+    public String getKeterangan() {
+        return keterangan;
+    }
+
+    public void setKeterangan(String keterangan) {
+        this.keterangan = keterangan;
+    }
+
+}
